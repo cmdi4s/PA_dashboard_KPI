043f67e7fd604b2ba8e66d266f2f26c0b58b97a8
cmdi4s <devvdias@gmail.com>
2026-02-10 15:37:33 -0300

    change db

--- dash/pages/Oficios.py
+++ dash/pages/Oficios.py
@@ -10,7 +10,7 @@ load_dotenv()
 def load_data():
     return pd.read_sql("SELECT * FROM test_table", engine)
 
-engine = create_engine(os.getenv("SUPABASE_DB_URL"))
+engine = create_engine(os.getenv("DB_URL"))
 
 
 df = load_data()
